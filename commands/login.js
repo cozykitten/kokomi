fbba1691211118e1522785fddb7aec90c13e95eb
cozykitten <116394977+cozykitten@users.noreply.github.com>
2022-12-26 10:23:22 -0600

    finish login command

--- commands/login.js
+++ commands/login.js
@@ -1,37 +1,32 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
 const crypto = require('crypto');
 const { cd, synccd } = require('../dbManager');
 require('dotenv').config();
 
 function encrypt(plain, globalPass) {
-
     const key = crypto.scryptSync(globalPass, process.env.SALT, 32);
     const iv = crypto.randomBytes(16);
     const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
 
     let encrypted = cipher.update(plain, "utf-8", "hex");
     encrypted += cipher.final("hex");
-    console.log(plain + '  ' + encrypted);
-    console.log(iv.toString('hex'))
     return { data: encrypted, iv: iv.toString('hex') };
 }
 
 function decrypt(secret, globalPass) {
-    console.log(secret)
     const key = crypto.scryptSync(globalPass, process.env.SALT, 32);
     //const iv = crypto.randomFillSync(new Uint8Array(16));
     const decipher = crypto.createDecipheriv('aes-256-cbc', key, Buffer.from(secret.iv, 'hex'));
 
     let decrypted = decipher.update(secret.data, "hex", "utf-8");
     decrypted += decipher.final("utf-8");
-    console.log(secret.data + ' ' + decrypted);
     return decrypted;
 }
 
 module.exports = {
     data: new SlashCommandBuilder()
-		.setName('login')
-		.setDescription('save and check your logins')
+        .setName('login')
+        .setDescription('save and check your logins')
         .addSubcommand(subcommand => subcommand.setName('add').setDescription('add a new login')
             .addStringOption(option => option.setName('globalpassword').setDescription('your password your logins are encrypted with').setRequired(true))
             .addStringOption(option => option.setName('service').setDescription('name of the service this login is for').setRequired(true))
@@ -42,6 +37,8 @@ module.exports = {
         .addSubcommand(subcommand => subcommand.setName('view').setDescription('display one of your saved logins')
             .addStringOption(option => option.setName('globalpassword').setDescription('your password your logins are encrypted with').setRequired(true))
             .addStringOption(option => option.setName('service').setDescription('name of the service this login is for').setRequired(true)))
+        .addSubcommand(subcommand => subcommand.setName('list').setDescription('list your saved services')
+            .addStringOption(option => option.setName('globalpassword').setDescription('your password your logins are encrypted with').setRequired(true)))
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
     async execute(interaction) {
@@ -57,36 +54,26 @@ module.exports = {
             const tag = interaction.options.getString('tag');
 
             const hash = crypto.createHash('sha256').update(interaction.options.getString('globalpassword') + process.env.SALT).digest('hex');
-            await interaction.reply({content: 'hashed', ephemeral: true});
-            console.log(hash);
 
-            if (cd[interaction.user.id]) {
-                if (cd[interaction.user.id].hash === hash) {
-                    console.log('hash identical');
-                    //addLogin(interaction.user.id, service);
-
-                    if (cd[interaction.user.id][service]) return interaction.editReply({ content: `You already saved a login for ${service}`});
-                    cd[interaction.user.id][service] = {};
-
-                    if (user) cd[interaction.user.id][service].user = encrypt(user, globalPass);
-                    if (email) cd[interaction.user.id][service].email = encrypt(email, globalPass);
-                    if (pass) cd[interaction.user.id][service].pass = encrypt(pass, globalPass);
-                    if (tag) cd[interaction.user.id][service].tag = encrypt(tag, globalPass);
-
-                    synccd(cd);
-                    console.log(cd);
-                }
+            if (!cd[interaction.user.id]) {
+                cd[interaction.user.id] = { hash: hash };
             }
             else {
-                cd[interaction.user.id] = { hash: hash };
-                //encrypt();
-                synccd(cd);
+                if (cd[interaction.user.id].hash !== hash) return interaction.reply({ content: `hashes don't match`, ephemeral: true });
             }
-            
 
-            
+            if (cd[interaction.user.id][service]) return interaction.reply({ content: `You already saved a login for ${service}`, ephemeral: true });
+            cd[interaction.user.id][service] = {};
 
-            //if (!users) return interaction.reply(`Topic "${interaction.options.getString('topic')}" doesn't exists.`);
+
+            if (user) cd[interaction.user.id][service].user = encrypt(user, globalPass);
+            if (email) cd[interaction.user.id][service].email = encrypt(email, globalPass);
+            if (pass) cd[interaction.user.id][service].pass = encrypt(pass, globalPass);
+            if (tag) cd[interaction.user.id][service].tag = encrypt(tag, globalPass);
+
+            synccd(cd);
+            return interaction.reply({ content: `I saved your login for ${service}`, ephemeral: true });
+
         }
         else if (interaction.options.getSubcommand() === 'view') {
 
@@ -94,19 +81,45 @@ module.exports = {
             const service = interaction.options.getString('service');
 
             const hash = crypto.createHash('sha256').update(globalPass + process.env.SALT).digest('hex');
-            await interaction.reply({content: 'hashed', ephemeral: true});
-            console.log(hash);
 
-            
-            interaction.editReply({ content: 'decrypted: ' + decrypt(cd[interaction.user.id][service].user, globalPass)});
+            if (!cd[interaction.user.id]) return interaction.reply({ content: `You didn't save any logins yet`, ephemeral: true });
+            if (cd[interaction.user.id].hash !== hash) return interaction.reply({ content: `hashes don't match`, ephemeral: true });
+            if (!cd[interaction.user.id][service]) return interaction.reply({ content: `You didn't save a login for ${service}`, ephemeral: true });
+
+
+            const plainText = [];
+            for (const field in cd[interaction.user.id][service]) {
+                plainText.push(field + ': ' + decrypt(cd[interaction.user.id][service][field], globalPass));
+            }
+
+            const replyEmbed = new EmbedBuilder()
+                .setTitle(`Your ${service} login`)
+                .setDescription(plainText.join('\n'))
+                .setColor('#797FCB')
+            interaction.reply({ embeds: [replyEmbed], ephemeral: true });
+
         }
+        else if (interaction.options.getSubcommand() === 'list') {
 
+            const globalPass = interaction.options.getString('globalpassword');
+            const hash = crypto.createHash('sha256').update(globalPass + process.env.SALT).digest('hex');
+            console.log(hash)
 
+            if (!cd[interaction.user.id]) return interaction.reply({ content: `You didn't save any logins yet`, ephemeral: true });
+            if (cd[interaction.user.id].hash !== hash) return interaction.reply({ content: `hashes don't match`, ephemeral: true });
+            
+            const plainText = [];
+            let notFirst = false;
+            for (const svc in cd[interaction.user.id]) {
+                if (notFirst) plainText.push(svc);
+                else notFirst = true;
+            }
 
-
-
-
-
-
+            const replyEmbed = new EmbedBuilder()
+                .setTitle('Your saved services')
+                .setDescription(plainText.join('\n'))
+                .setColor('#797FCB')
+                interaction.reply({ embeds: [replyEmbed], ephemeral: true });
+        }
     }
 }
