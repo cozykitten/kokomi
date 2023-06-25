bc586ac94d17e5b2546f4f81a6e6fae43d8b2636
cozykitten <116394977+cozykitten@users.noreply.github.com>
2023-06-25 10:11:36 -0500

    update

--- commands/ping.js
+++ commands/ping.js
@@ -1,13 +1,34 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('ping Kokomi'),
-	async execute(message){
+	async execute(interaction){
+		
+		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true, ephemeral: true });
+		const botLatency = (sent.createdTimestamp - interaction.createdTimestamp) + 'ms';
+		let apiLatency = interaction.client.ws.ping;
+		if (apiLatency < 0) {
+			apiLatency = 'unavailable';
+		}
+		else{
+			apiLatency += 'ms';
+		}
 
-		if (message.commandName) await message.reply({ content: 'did you expect me to say \'pong\' now?', ephemeral: true});
-		else message.channel.send('did you expect me to say \'pong\' now?');
-
+		const embed = new EmbedBuilder()
+			.setTitle('Ping')
+			.setColor(0x797FCB)
+			.addFields({ 
+				name: 'Latency',
+				value: botLatency,
+				inline: true
+			},
+			{
+				name: 'API Latency',
+				value: apiLatency,
+				inline: true
+			});
+		interaction.editReply({ embeds: [embed] });
 	}
 }
