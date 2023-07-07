7bce9a26e16e3fd1031149f11fa82b97b02e0792
cozykitten <116394977+cozykitten@users.noreply.github.com>
2023-07-07 16:50:07 -0500

    adding authentication to github request

--- src/onceReady.js
+++ src/onceReady.js
@@ -1,3 +1,4 @@
+const { EmbedBuilder } = require('discord.js');
 require('dotenv').config();
 const { db, lb, synclb } = require('./dbManager');
 
@@ -22,9 +23,11 @@ module.exports = async (client) => {
     client.commands.get('reminder').loadReminders(client);
 
     //checking git
-    githubTimed(client);
+    console.log('checking github..');
+    githubTimed(client, log);
 
     //checking twitch
+    console.log('checking twitch..');
     const twitchCache = {};
     twitchTimed(client, twitchCache);
 }
@@ -36,9 +39,20 @@ module.exports = async (client) => {
  * 
  * @param {Discord.Client} client Discord Client.
  */
-async function githubTimed(client) {
+async function githubTimed(client, channel) {
     //TODO: export in own command file that allows adding and removing of repos to check, similar to reminder command
-    console.log('checking github..');
+    const isTokenValid = await checkGithubTokenExpiration();
+
+    if (!isTokenValid) {
+        console.log('GitHub token is expired or invalid. Please generate a new token.');
+        const embed = new EmbedBuilder()
+            .setTitle('Invalid token')
+            .setColor(0xc43838)
+            .setDescription('GitHub token is expired or invalid. Please generate a new token.');
+        channel.send({ embeds: [embed] });
+        return;
+    }
+
     for (const uid in db.github) {
         const discordUser = await client.users.fetch(uid);
         for (const e of db.github[uid]) {
@@ -60,15 +74,25 @@ async function githubTimed(client) {
  */
 async function twitchTimed(client, twitchCache) {
     //TODO: export in own command file that allows adding and removing of channels to check, similar to reminder command
-    console.log('checking twitch..');
+    const clientId = process.env.TWITCH_ID;
+    const clientSecret = process.env.TWITCH_SECRET;
+    const accessToken = await getTwitchAccessToken(clientId, clientSecret);
+
     for (const uid in db.twitch) {
 
         const discordUser = await client.users.fetch(uid);
         for (const e of db.twitch[uid]) {
 
-            if (await twitch(e, twitchCache)) {
+            const stream = await twitch(e, twitchCache, clientId, accessToken);
+            if (stream) {
                 try {
-                    await discordUser.send({ content: `${e} is live now on https://www.twitch.tv/${e}` });
+                    const embed = new EmbedBuilder()
+                        .setTitle('Twitch')
+                        .setColor(0x797FCB)
+                        .setDescription(`${e} is now live on twitch!`)
+                        .setURL(`https://www.twitch.tv/${e}`)
+                        .setImage(stream.replace('{width}x{height}', '1280x720'));
+                    await discordUser.send({ embeds: [embed] });
                 } catch (e) {
                     console.error("Cannot send messages to " + discordUser.username);
                     const server = await client.guilds.cache.get(process.env.KOKOMI_HOME); 
@@ -135,35 +159,39 @@ async function checkLastExit(log) {
  * 
  * @param {String} owner The owner of the github repository to check.
  * @param {String} repo The github repository to check.
- * @param {Discord.User} discordUser 
+ * @param {Discord.User} discordUser The Discord user to send a message to.
  */
 async function github(owner, repo, discordUser) {
     // Retrieve information on the latest release
-    fetch(`https://api.github.com/repos/${owner}/${repo}/releases?per_page=1`)
-        .then(response => response.json())
-        .then(async latestRelease => {
-            const latestReleaseTime = Date.parse(latestRelease[0].published_at);
-
-            // Check if there is a new release since the past 24h
-            const currentTime = Date.now();
-            if (latestReleaseTime > (currentTime - 86400000)) {
-                try {
-                    await discordUser.send(`New release available: ${latestRelease[0].name}\nhttps://github.com/${owner}/${repo}/releases`);
-                } catch (e) {
-                    console.error("Cannot send messages to " + discordUser.username);
-                    const server = await client.guilds.cache.get(process.env.KOKOMI_HOME); 
-                    const channel = await server.channels.cache.get(process.env.KOKOMI_LOG);
-                    channel.send("Cannot send messages to " + discordUser.username);
-                }
+    const url = `https://api.github.com/repos/${owner}/${repo}/releases?per_page=1`;
+    try {
+        const response = await fetch(url, {
+            headers: {
+                Authorization: `Bearer ${process.env.GITHUB_TOKEN}`
             }
-            else {
-                return;
-            }
-        })
-        .catch(error => {
-            console.log(`Error retrieving release information: ${error}`);
-            return;
         });
+
+        const latestRelease = await response.json();
+        const latestReleaseTime = Date.parse(latestRelease[0].published_at);
+
+        // Check if there is a new release since the past 24h
+        const currentTime = Date.now();
+        if (latestReleaseTime > (currentTime - 86400000)) {
+            try {
+                await discordUser.send(`New release available: ${latestRelease[0].name}\nhttps://github.com/${owner}/${repo}/releases`);
+            } catch (e) {
+                console.error("Cannot send messages to " + discordUser.username);
+                const server = await client.guilds.cache.get(process.env.KOKOMI_HOME);
+                const channel = await server.channels.cache.get(process.env.KOKOMI_LOG);
+                channel.send("Cannot send messages to " + discordUser.username);
+            }
+        } else {
+            return;
+        }
+    } catch (error) {
+        console.log(`Error retrieving release information: ${error}`);
+        return;
+    };
 }
 
 /**
@@ -171,13 +199,11 @@ async function github(owner, repo, discordUser) {
  * 
  * @param {String} channelName Name of the twitch channel to check.
  * @param {Object.<string, number>} twitchCache Cache holding the stream id for each channel if it's live.
- * @returns {boolean} Returns true only once for each stream id.
+ * @param {String} clientId Twitch client id.
+ * @param {String} accessToken Twitch access token.
+ * @returns {String} Returns the thumbnail url only once for each stream id.
  */
-async function twitch(channelName, twitchCache) {
-
-    const clientId = process.env.TWITCH_ID;
-    const clientSecret = process.env.TWITCH_SECRET;
-    const accessToken = await getAccessToken(clientId, clientSecret);
+async function twitch(channelName, twitchCache, clientId, accessToken) {
     const url = `https://api.twitch.tv/helix/streams?user_login=${channelName}`;
 
     try {
@@ -192,11 +218,10 @@ async function twitch(channelName, twitchCache) {
         if (data.data.length > 0) {
             // Channel is live, cache stream id
             const stream = data.data[0];
-
             // If the same stream id is already set, that means this function already returned true before for this stream id
             if (!(twitchCache[channelName] === stream.id)) {
                 twitchCache[channelName] = stream.id;
-                return true;
+                return stream.thumbnail_url;
             }
             return false;
         }
@@ -205,24 +230,45 @@ async function twitch(channelName, twitchCache) {
             twitchCache[channelName] = 0;
             return false;
         }
-    } catch (error) {
+    }
+    catch (error) {
         console.error('Error:', error);
         return false;
     }
+}
 
-    async function getAccessToken(clientId, clientSecret) {
-        const url = 'https://id.twitch.tv/oauth2/token';
-        const params = new URLSearchParams();
-        params.append('client_id', clientId);
-        params.append('client_secret', clientSecret);
-        params.append('grant_type', 'client_credentials');
-    
-        const response = await fetch(url, {
-            method: 'POST',
-            body: params
+async function checkGithubTokenExpiration() {
+    try {
+        const response = await fetch('https://api.github.com/user', {
+            headers: {
+                Authorization: `Bearer ${process.env.GITHUB_TOKEN}`
+            }
         });
-    
-        const data = await response.json();
-        return data.access_token;
+
+        if (response.status === 401) {
+            return false; // Token is expired or invalid
+        }
+
+        return true; // Token is valid
     }
+    catch (error) {
+        console.log(`Error checking token expiration: ${error}`);
+        return false; // Error occurred, assume token is expired or invalid
+    }
+}
+
+async function getTwitchAccessToken(clientId, clientSecret) {
+    const url = 'https://id.twitch.tv/oauth2/token';
+    const params = new URLSearchParams();
+    params.append('client_id', clientId);
+    params.append('client_secret', clientSecret);
+    params.append('grant_type', 'client_credentials');
+
+    const response = await fetch(url, {
+        method: 'POST',
+        body: params
+    });
+
+    const data = await response.json();
+    return data.access_token;
 }
